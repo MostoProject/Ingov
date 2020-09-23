b48468dd2459709d617abf961beeaaacf7e09d32
MostoProject <71740580+MostoProject@users.noreply.github.com>
2020-09-23 13:34:35 +0700

    Add files via upload

---
+++ startup.py
@@ -0,0 +1,14 @@
+'''
+| Startup @04 September 2020
+| : Execute this file
+| : For run application
+'''
+from codeartists import CodeArtists
+
+if __name__ == "__main__":
+    '''
+    | Calling main application
+    | : Build main application
+    '''
+    codeArtists = CodeArtists()
+    codeArtists.build()
